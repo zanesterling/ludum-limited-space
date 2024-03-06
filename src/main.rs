a7106f19cefbec96a7f3479b0cf6f65b5f35ccf6
Zane Sterling <sterling.zane@gmail.com>
2024-03-06 19:25:46 +0100

    Add bevy_rapier3d for physics

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,35 @@
 use bevy::prelude::*;
+use bevy_rapier3d::prelude::*;
 
 const BACKGROUND_COLOR: Color = Color::BLACK;
 
 fn main() {
   App::new()
     .add_plugins(DefaultPlugins)
+    .add_plugins(RapierPhysicsPlugin::<NoUserData>::default())
+    .add_plugins(RapierDebugRenderPlugin::default())
     .insert_resource(ClearColor(BACKGROUND_COLOR))
-    .add_systems(Startup, setup)
-    .add_systems(FixedUpdate, move_sprite)
+    .add_systems(Startup, (setup_graphics, setup_physics))
     .run();
 }
 
-fn setup(mut commands: Commands, asset_server: Res<AssetServer>) {
-  commands.spawn(Camera2dBundle::default());
-  commands.spawn((
-    SpriteBundle {
-      texture: asset_server.load("bevy_bird_dark.png"),
-      transform: Transform::from_xyz(100., 0., 0.),
-      ..default()
-    },
-    Player,
-  ));
+fn setup_graphics(mut commands: Commands) {
+  commands.spawn(Camera3dBundle {
+    transform: Transform::from_xyz(-3.0, 3.0, 10.0).looking_at(Vec3::ZERO, Vec3::Y),
+    ..Default::default()
+  });
 }
 
-const PLAYER_SPEED: f32 = 500.;
+fn setup_physics(mut commands: Commands) {
+  /* Create the ground. */
+  commands
+    .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+    .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
 
-fn move_sprite(
-  keyboard_input: Res<ButtonInput<KeyCode>>,
-  mut query: Query<&mut Transform, With<Player>>,
-  time: Res<Time>,
-) {
-  let mut player_transform = query.single_mut();
-  let mut direction = Vec2::new(0., 0.);
-
-  if keyboard_input.pressed(KeyCode::ArrowLeft) {
-    direction.x -= 1.0;
-  }
-  if keyboard_input.pressed(KeyCode::ArrowRight) {
-    direction.x += 1.0;
-  }
-  if keyboard_input.pressed(KeyCode::ArrowDown) {
-    direction.y -= 1.0;
-  }
-  if keyboard_input.pressed(KeyCode::ArrowUp) {
-    direction.y += 1.0;
-  }
-
-  if !(direction.x == 0.0 && direction.y == 0.0) {
-    let velocity = direction.normalize() * PLAYER_SPEED * time.delta_seconds();
-    player_transform.translation.x += velocity.x;
-    player_transform.translation.y += velocity.y;
-  }
+  /* Create the bouncing ball. */
+  commands
+    .spawn(RigidBody::Dynamic)
+    .insert(Collider::ball(0.5))
+    .insert(Restitution::coefficient(0.7))
+    .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
 }
-
-#[derive(Component)]
-struct Player;
