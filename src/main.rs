e80b4cdad81ab03c9d7923ab44df521d242850d7
Zane Sterling <sterling.zane@gmail.com>
2024-03-07 18:50:58 +0100

    Remove the bouncing ball.
    It was just getting annoying.

--- src/main.rs
+++ src/main.rs
@@ -27,13 +27,6 @@ fn setup_physics(mut commands: Commands) {
     .spawn(Collider::cuboid(100.0, 0.1, 100.0))
     .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.0, 0.0)));
 
-  /* Create the bouncing ball. */
-  commands
-    .spawn(RigidBody::Dynamic)
-    .insert(Collider::ball(0.5))
-    .insert(Restitution::coefficient(0.7))
-    .insert(TransformBundle::from(Transform::from_xyz(0.0, 4.0, 0.0)));
-
   /* Configure the character controller & collider. */
   commands
     .spawn(RigidBody::KinematicPositionBased)
