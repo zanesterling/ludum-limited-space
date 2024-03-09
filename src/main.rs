cd20d0d22b29d6b8d8ad57eeab23f49217af22ab
Zane Sterling <sterling.zane@gmail.com>
2024-03-09 10:50:48 +0100

    Make a second separately controllable player

--- src/main.rs
+++ src/main.rs
@@ -47,8 +47,18 @@ fn setup_physics(mut commands: Commands) {
   /* Configure the character controller & collider. */
   commands
     .spawn(RigidBody::KinematicPositionBased)
+    .insert(PlayerKeys::PLAYER_1)
     .insert(Collider::ball(0.5))
     .insert(TransformBundle::from(Transform::from_xyz(0.0, 1.0, 0.0)))
+    .insert(KinematicCharacterController {
+      offset: CharacterLength::Relative(0.01),
+      ..default()
+    });
+  commands
+    .spawn(RigidBody::KinematicPositionBased)
+    .insert(PlayerKeys::PLAYER_2)
+    .insert(Collider::ball(0.5))
+    .insert(TransformBundle::from(Transform::from_xyz(2.0, 1.0, 0.0)))
     .insert(KinematicCharacterController {
       offset: CharacterLength::Relative(0.01),
       ..default()
@@ -58,26 +68,49 @@ fn setup_physics(mut commands: Commands) {
 const PLAYER_SPEED: f32 = 5.0;
 fn update_system(
   keyboard_input: Res<ButtonInput<KeyCode>>,
-  mut controllers: Query<&mut KinematicCharacterController>,
+  mut controllers: Query<(&mut KinematicCharacterController, &PlayerKeys)>,
   time: Res<Time>,
 ) {
-  for mut controller in controllers.iter_mut() {
+  for (mut controller, keybinds) in controllers.iter_mut() {
     let gravity_drop = Vec3::new(0.0, -3.0, 0.0);
     let mut direction = Vec3::new(0.0, 0.0, 0.0);
-    if keyboard_input.pressed(KeyCode::ArrowLeft) {
+    if keyboard_input.pressed(keybinds.left) {
       direction.x -= 1.0;
     }
-    if keyboard_input.pressed(KeyCode::ArrowRight) {
+    if keyboard_input.pressed(keybinds.right) {
       direction.x += 1.0;
     }
-    if keyboard_input.pressed(KeyCode::ArrowUp) {
+    if keyboard_input.pressed(keybinds.up) {
       direction.z -= 1.0;
     }
-    if keyboard_input.pressed(KeyCode::ArrowDown) {
+    if keyboard_input.pressed(keybinds.down) {
       direction.z += 1.0;
     }
 
     let velocity = direction * PLAYER_SPEED + gravity_drop;
     controller.translation = Some(velocity * time.delta_seconds());
   }
+}
+
+#[derive(Component)]
+struct PlayerKeys {
+  up: KeyCode,
+  down: KeyCode,
+  left: KeyCode,
+  right: KeyCode,
+}
+
+impl PlayerKeys {
+  const PLAYER_1: PlayerKeys = PlayerKeys {
+    up: KeyCode::ArrowUp,
+    down: KeyCode::ArrowDown,
+    left: KeyCode::ArrowLeft,
+    right: KeyCode::ArrowRight,
+  };
+  const PLAYER_2: PlayerKeys = PlayerKeys {
+    up: KeyCode::KeyW,
+    down: KeyCode::KeyS,
+    left: KeyCode::KeyA,
+    right: KeyCode::KeyD,
+  };
 }
