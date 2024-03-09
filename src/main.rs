0c771dc98f7a47d163712cae1bc29d6137149035
Zane Sterling <sterling.zane@gmail.com>
2024-03-09 12:06:06 +0100

    Point the balls at the mouse

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use bevy::prelude::*;
+use bevy::window::PrimaryWindow;
 use bevy_rapier3d::prelude::*;
 
 const BACKGROUND_COLOR: Color = Color::BLACK;
@@ -10,21 +11,24 @@ fn main() {
     .add_plugins(RapierDebugRenderPlugin::default())
     .insert_resource(ClearColor(BACKGROUND_COLOR))
     .add_systems(Startup, (setup_graphics, setup_physics))
-    .add_systems(FixedUpdate, update_player)
+    .add_systems(FixedUpdate, (update_player, point_to_cursor))
     .run();
 }
 
 fn setup_graphics(mut commands: Commands) {
-  commands.spawn(Camera3dBundle {
-    transform: Transform::from_xyz(0.0, 20.0, 0.0).looking_at(Vec3::ZERO, -Vec3::Z),
-    ..Default::default()
-  });
+  commands
+    .spawn(Camera3dBundle {
+      transform: Transform::from_xyz(0.0, 20.0, 0.0).looking_at(Vec3::ZERO, -Vec3::Z),
+      ..Default::default()
+    })
+    .insert(GameCamera);
 }
 
 fn setup_physics(mut commands: Commands) {
   /* Create the ground. */
   commands
     .spawn(Collider::cuboid(100.0, 0.1, 100.0))
+    .insert(GroundPlane)
     .insert(TransformBundle::from(Transform::from_xyz(0.0, -2.1, 0.0)));
 
   /* Create some walls for the player to bump into. */
@@ -113,4 +117,48 @@ impl PlayerKeys {
     left: KeyCode::KeyA,
     right: KeyCode::KeyD,
   };
+}
+
+#[derive(Component)]
+struct GameCamera;
+
+#[derive(Component)]
+struct GroundPlane;
+
+fn point_to_cursor(
+  q_window: Query<&Window, With<PrimaryWindow>>,
+  q_camera: Query<(&Camera, &GlobalTransform), With<GameCamera>>,
+  q_plane: Query<&GlobalTransform, With<GroundPlane>>,
+  mut q_player_transform: Query<&mut Transform, With<KinematicCharacterController>>,
+) {
+  let window = q_window.single();
+  let (camera, camera_transform) = q_camera.single();
+  let ground_transform = q_plane.single();
+
+  // Get the cursor, but give up if it's outside the window.
+  let Some(cursor_position) = window.cursor_position() else {
+    return;
+  };
+
+  // Get a ray from the camera to the cursor position.
+  let Some(ray) = camera.viewport_to_world(camera_transform, cursor_position) else {
+    // If it was impossible to compute, do nothing.
+    return;
+  };
+
+  // Intersect the view ray and the ground plane.
+  let plane_origin = ground_transform.translation();
+  let plane = Plane3d::new(ground_transform.up());
+  let Some(distance) = ray.intersect_plane(plane_origin, plane) else {
+    // If the ray doesn't intersect the ground (camera is looking away),
+    // do nothing.
+    return;
+  };
+  let intersection = ray.get_point(distance);
+
+  for mut transform in q_player_transform.iter_mut() {
+    let mut to_mouse = transform.translation - intersection;
+    to_mouse.y = 0.0;
+    transform.rotation = Quat::from_rotation_arc(-Vec3::Z, to_mouse.normalize());
+  }
 }
